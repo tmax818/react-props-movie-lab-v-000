b7ec84a1674b5af6c9fa868f46be6cc7f22806a5
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-09-10 15:33:59 -0700

    init commit

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import MovieCard from './card-components/MovieCard.js'
-import movieData from './data.js'
+import MovieCard from './card-components/MovieCard.js';
+import movieData from './data.js';
 
 export default class MovieShowcase extends Component {
-
   generateMovieCards = () => {
-    // map over your movieData array and return the correct 
-  }
+    // map over your movieData array and return the correct
+    let arr = movieData.map(mov => {
+      return (
+        <MovieCard
+          title={mov.title}
+          IMDBRating={mov.IMDBRating}
+          genres={mov.genres}
+          poster={mov.poster}
+        />
+      );
+    });
+    return arr;
+  };
 
   render() {
-    return (
-      <div id="movie-showcase">
-        {this.generateMovieCards()}
-      </div>
-    )
+    return <div id="movie-showcase">{this.generateMovieCards()}</div>;
   }
 }
--- src/card-components/CardFront.js
+++ src/card-components/CardFront.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 export default class CardFront extends Component {
-
   render() {
+    console.log(this.props);
     return (
-      <div className="card-front" style={{backgroundImage: `url(${''})`}}>
-      </div>
-    )
+      <div
+        className="card-front"
+        style={{ backgroundImage: `url(${this.props.poster})` }}
+      />
+    );
   }
 }
